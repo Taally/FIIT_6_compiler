81a2a2401d41c023d8bcff436cdac701c05dea76
Ukolnir <metallwind@gmail.com>
2020-03-30 13:38:27 +0300

    Added mult opt

--- Main.cs
+++ Main.cs
@@ -29,12 +29,25 @@ namespace SimpleCompiler{
                     string output = JsonConvert.SerializeObject(parser.root, jsonSettings);
                     File.WriteAllText(OutputFileName, output);
 
+                    Console.WriteLine("Before opt");
                     var pp = new PrettyPrintVisitor();
                     parser.root.Visit(pp);
                     Console.WriteLine(pp.Text);
 
                     var fp = new FillParentVisitor();
                     parser.root.Visit(fp);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine("After opt");
+
+                    var opt = new OptVisitor();
+                    parser.root.Visit(opt);
+
+                    var pp1 = new PrettyPrintVisitor();
+                    parser.root.Visit(pp1);
+                    Console.WriteLine(pp1.Text);
                 }
             }
             catch (FileNotFoundException){
