0941e7dffc6133c37ec259524652b9cabdbadf2c
Dmitry Volodin <mr.molkree@gmail.com>
2020-05-01 19:51:13 +0300

    Added test project

---
+++ SimpleLanguage.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+ 
+namespace SimpleLanguage.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(1 == 1);
+        }
+    }
+}
