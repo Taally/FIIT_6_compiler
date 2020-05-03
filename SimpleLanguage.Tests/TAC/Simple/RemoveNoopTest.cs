825732135a167c8059ad6893b028a6cf4e54ecd5
xittz <a.patseev@gmail.com>
2020-05-03 16:08:29 +0300

    RemoveNoopTests + bugfix

--- SimpleLanguage.Tests/TAC/Simple/RemoveNoopTest.cs
+++ SimpleLanguage.Tests/TAC/Simple/RemoveNoopTest.cs
@@ -3,14 +3,111 @@ using SimpleLang;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using NUnit.Framework.Internal;
 
 namespace SimpleLanguage.Tests.TAC.Simple
 {
     [TestFixture]
     class RemoveNoopTest : TACTestsBase
     {
+        public Tuple<bool, List<Instruction>> OptimizeLocal(List<Instruction> tac)
+        {
+            return ThreeAddressCodeRemoveNoop.RemoveEmptyNodes(tac);
+        }
+
+        public void AssertChanged(Tuple<bool, List<Instruction>> result, List<String> expected)
+        {
+            Assert.IsTrue(result.Item1);
+            CollectionAssert.AreEqual(result.Item2.Select(x => x.ToString()), expected);
+        }
+
+        public void AssertNotChanged(Tuple<bool, List<Instruction>> result, List<String> expected)
+        {
+            Assert.IsFalse(result.Item1);
+            CollectionAssert.AreEqual(result.Item2.Select(x => x.ToString()), expected);
+        }
+        
         [Test]
-        public void Test1()
+        public void ShouldNotRemoveLastNoop()
+        {
+            var TAC = new List<Instruction>
+            {
+                new Instruction("6", "assign", "b", "", "a"),
+                new Instruction("", "noop", null, null, null)
+            };
+            var result = OptimizeLocal(TAC);
+            var expected = new List<String>
+            {
+                "6: a = b",
+                "noop"
+            };
+            
+            AssertNotChanged(result, expected);
+        }
+
+        [Test]
+        public void ShouldRemoveOnlyOneNoop()
+        {
+            var TAC = new List<Instruction>
+            {
+                new Instruction("6", "assign", "b", "", "a"),
+                new Instruction("1", "noop", null, null, null),
+                new Instruction("9", "assign", "a", "", "b"),
+                new Instruction("", "noop", null, null, null),
+            };
+
+            var result = OptimizeLocal(TAC);
+            
+            var expected = new List<string>()
+            {
+                "6: a = b",
+                "9: b = a",
+                "noop"
+            };
+            
+            AssertChanged(result, expected);
+        }
+
+        [Test]
+        public void ShouldConcatNoopWithLabelWithNextOpIfItHasNoLabel()
+        {
+            var TAC = new List<Instruction>
+            {
+                new Instruction("L1", "noop", "", "", ""),
+                new Instruction("", "assign", "b", "", "a")
+            };
+            
+            var expected = new List<string>()
+            {
+                "L1: a = b"
+            };
+            
+            AssertChanged(OptimizeLocal(TAC), expected);
+        }
+
+        [Test]
+        public void ShouldRenameGotosToNoopWithLabelWhenNextIsLabeled()
+        {
+            var TAC = new List<Instruction>
+            {
+                new Instruction("", "goto", "old_label", "", ""),
+                new Instruction("old_label", "noop", "", "", ""),
+                new Instruction("new_label", "assign", "a", "", "b"),
+                new Instruction("", "goto", "old_label", "", "")
+            };
+
+            var expected = new List<String>
+            {
+                "goto new_label",
+                "new_label: b = a",
+                "goto new_label",
+            };
+            
+            AssertChanged(OptimizeLocal(TAC), expected);
+        }
+
+        [Test]
+        public void ShouldRemoveAllTheNoops()
         {
             var TAC = new List<Instruction>
             {
@@ -23,18 +120,15 @@ namespace SimpleLanguage.Tests.TAC.Simple
                 new Instruction("7", "noop", null, null, null),
                 new Instruction("8", "noop", null, null, null),
             };
-            ThreeAddressCodeOptimizer.Optimizations.Clear();
-            ThreeAddressCodeOptimizer.Optimizations.Add(ThreeAddressCodeRemoveNoop.RemoveEmptyNodes);
-
+           
             var expected = new List<string>()
             {
                 "3: a = 1",
                 "6: b = a",
+                "8: noop"
             };
-            var actual = ThreeAddressCodeOptimizer.Optimize(TAC)
-                .Select(instruction => instruction.ToString());
-
-            CollectionAssert.AreEqual(expected, actual);
+           
+            AssertChanged(OptimizeLocal(TAC), expected);
         }
     }
 }
