97d38ceb6a84112de7e9caccb053d512806de052
Ukolnir <metallwind@gmail.com>
2020-05-04 13:58:40 +0300

    Update OptExprSubEqualVar.cs

--- Visitors/ExprOptimizations/OptExprSubEqualVar.cs
+++ Visitors/ExprOptimizations/OptExprSubEqualVar.cs
@@ -8,9 +8,9 @@ namespace SimpleLang.Visitors
         public override void PostVisit(Node n)
         {
             // a - a => 0
-            if (n is BinOpNode binop && binop.Op == OpType.MINUS)
-            {
-                if (binop.Left is IdNode id1 && binop.Right is IdNode id2 && id1.Name == id2.Name)
+            if (n is BinOpNode binop && binop.Op == OpType.MINUS 
+                && binop.Left is IdNode id1 && binop.Right is IdNode id2){
+                if (id1.Name == id2.Name)
                 { 
                     ReplaceExpr(binop, new IntNumNode(0));
                 }
