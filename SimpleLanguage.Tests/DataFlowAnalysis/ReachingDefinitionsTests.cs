1040d25ddcaddfce8338e2ead337d2b2ad0031a8
Dmitry Volodin <mr.molkree@gmail.com>
2020-05-21 23:30:40 +0300

    Moved Reaching definitions tests to DataFlowAnalysis namespace

--- SimpleLanguage.Tests/DataFlowAnalysis/ReachingDefinitionsTests.cs
+++ SimpleLanguage.Tests/DataFlowAnalysis/ReachingDefinitionsTests.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using static SimpleLang.ReachingDefinitions;
 
-namespace SimpleLanguage.Tests.CFG
+namespace SimpleLanguage.Tests.DataFlowAnalysis
 {
     [TestFixture]
     class ReachingDefinitionsTests : TACTestsBase
