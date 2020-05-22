b0c970e68f93e9e05e1838ebc4104ea8a87ad522
Ukolnir <metallwind@gmail.com>
2020-05-22 15:56:37 +0300

    Add LiveVariableTransferFunc

--- DataFlowAnalysis/LiveVariableAnalysis.cs
+++ DataFlowAnalysis/LiveVariableAnalysis.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SimpleLang
-{
+namespace SimpleLang{
     public class InOutSet {
         public HashSet<string> IN { get; set; }
         public HashSet<string> OUT { get; set; }
@@ -46,20 +45,15 @@ namespace SimpleLang
         }
     }
 
-
     public class LiveVariableAnalysis{
-        Stack<BasicBlock> st; 
         public Dictionary<int, InOutSet> dictInOut; 
-        Dictionary<int, DefUseSet> dictDefUse;
 
         public void Execute(ControlFlowGraph cfg) {
             var blocks = cfg.GetCurrentBasicBlocks();
+            var transferFunc = new LiveVariableTransferFunc(cfg);
 
-            foreach (var x in blocks) {
-                int n = cfg.VertexOf(x);
-                dictInOut.Add(n,new InOutSet());
-                dictDefUse.Add(n, new DefUseSet(FillDefUse(x.GetInstructions())));
-            }
+            foreach (var x in blocks)
+                dictInOut.Add(cfg.VertexOf(x),new InOutSet());
 
             bool isChanged = true;
             while (isChanged){
@@ -73,39 +67,14 @@ namespace SimpleLang
                         .Aggregate(new HashSet<string>(), (a, b) => a.Union(b).ToHashSet());
 
                     var pred = dictInOut[i].IN;
-
-                    dictInOut[i].IN =
-                        (new HashSet<string>().Union(dictDefUse[i].use))
-                        .Union(dictInOut[i].OUT.Except(dictDefUse[i].def))
-                        .ToHashSet();
-
+                    dictInOut[i].IN = transferFunc.Transfer(blocks[i], dictInOut[i].OUT);
                     isChanged = !dictInOut[i].IN.SetEquals(pred) || isChanged;
                 }
             }
         }
 
         public LiveVariableAnalysis() {
-            st = new Stack<BasicBlock>();
             dictInOut = new Dictionary<int, InOutSet>();
-            dictDefUse = new Dictionary<int, DefUseSet>();
-        }
-
-        Func<string, bool> IsId = ThreeAddressCodeDefUse.IsId;
-
-        public (HashSet<string> def, HashSet<string> use) FillDefUse(List<Instruction> block) {
-            HashSet<string> def = new HashSet<string>();
-            HashSet<string> use = new HashSet<string>();
-            for (int i = 0; i < block.Count; ++i) {
-                var inst = block[i];
-
-                if (IsId(inst.Argument1) && !def.Contains(inst.Argument1))
-                    use.Add(inst.Argument1);
-                if (IsId(inst.Argument2) && !def.Contains(inst.Argument2))
-                    use.Add(inst.Argument2);
-                if (IsId(inst.Result) && !use.Contains(inst.Result))
-                    def.Add(inst.Result);
-            }
-            return (def, use);
         }
 
         public string ToString(ControlFlowGraph cfg){
@@ -117,16 +86,6 @@ namespace SimpleLang
                 foreach (var b in x.GetInstructions()) {
                     str.Append(b.ToString() + "\n");
                 }
-                str.Append($"\n\n---use set---\n");
-                str.Append("{");
-                foreach (string i in dictDefUse[n].use)
-                    str.Append($" {i}");
-                str.Append(" }");
-                str.Append($"\n\n---def set---\n");
-                str.Append("{");
-                foreach (string i in dictDefUse[n].def)
-                    str.Append($" {i}");
-                str.Append(" }");
                 str.Append($"\n\n---IN set---\n");
                 str.Append("{");
                 foreach (string i in dictInOut[n].IN)
@@ -140,5 +99,39 @@ namespace SimpleLang
             }
             return str.ToString();
         }
+    }
+
+    public class LiveVariableTransferFunc : ITransFunc<HashSet<string>> {
+        Dictionary<BasicBlock, DefUseSet> dictDefUse;
+
+        private (HashSet<string> def, HashSet<string> use) FillDefUse(List<Instruction> block)
+        {
+            Func<string, bool> IsId = ThreeAddressCodeDefUse.IsId;
+
+            HashSet<string> def = new HashSet<string>();
+            HashSet<string> use = new HashSet<string>();
+            for (int i = 0; i < block.Count; ++i)
+            {
+                var inst = block[i];
+
+                if (IsId(inst.Argument1) && !def.Contains(inst.Argument1))
+                    use.Add(inst.Argument1);
+                if (IsId(inst.Argument2) && !def.Contains(inst.Argument2))
+                    use.Add(inst.Argument2);
+                if (IsId(inst.Result) && !use.Contains(inst.Result))
+                    def.Add(inst.Result);
+            }
+            return (def, use);
+        }
+
+        public LiveVariableTransferFunc(ControlFlowGraph cfg) {
+            var blocks = cfg.GetCurrentBasicBlocks();
+            dictDefUse = new Dictionary<BasicBlock, DefUseSet>();
+            foreach (var x in blocks)
+                dictDefUse.Add(x, new DefUseSet(FillDefUse(x.GetInstructions())));
+        }
+
+        public HashSet<string> Transfer(BasicBlock basicBlock, HashSet<string> OUT) =>
+            dictDefUse[basicBlock].use.Union(OUT.Except(dictDefUse[basicBlock].def)).ToHashSet();
     }
 }
