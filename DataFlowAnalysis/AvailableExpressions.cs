1c1889cf78ae1a0214bbae66829ecc71621c476f
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-05-25 10:05:43 +0300

    Update AvailableExpressions.cs

--- DataFlowAnalysis/AvailableExpressions.cs
+++ DataFlowAnalysis/AvailableExpressions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,43 @@ using System.Threading.Tasks;
 
 namespace SimpleLang
 {
+    public class AvailableExpressionsTransferFunc : ITransFunc<List<Instruction>>
+    {
+        Dictionary<BasicBlock, List<Instruction>> e_gen;
+        Dictionary<BasicBlock, List<Instruction>> e_kill;
+
+        public AvailableExpressionsTransferFunc(ControlFlowGraph graph)
+        {
+            var availableExpr = new AvailableExpressions(graph);
+            this.e_gen = GetInstrFromSet(availableExpr.e_gen);
+            this.e_kill = GetInstrFromSet(availableExpr.e_kill);
+        }
+
+        //e_gen U (in - e_kill)
+
+        public List<Instruction> Transfer(BasicBlock basicBlock, List<Instruction> input)
+            => e_gen[basicBlock]
+                .Union(input.Except(e_kill[basicBlock]))
+                .ToList();
+
+        private Dictionary<BasicBlock, List<Instruction>> GetInstrFromSet
+            (Dictionary<BasicBlock, List<Expression>> setOfExpressions)
+        {
+            var setOfInstructions = new Dictionary<BasicBlock, List<Instruction>>();
+            foreach(var expression in setOfExpressions)
+            {
+                var listOfInstructions = new List<Instruction>();
+                foreach(var expr in expression.Value)
+                    listOfInstructions.Add(expr.GetInstruction());
+                setOfInstructions[expression.Key] = new List<Instruction>(listOfInstructions);
+            }
+            return setOfInstructions;
+        }
+    }
+
     public class Expression
     {
+        private readonly Instruction instruction;
         public string Operation { get; }
         public string Argument1 { get; }
         public string Argument2 { get; }
@@ -17,8 +52,12 @@ namespace SimpleLang
             Operation = instruction.Operation;
             Argument1 = instruction.Argument1;
             Argument2 = instruction.Argument2;
+            this.instruction = instruction;
         }
-
+        public Instruction GetInstruction()
+        {
+            return instruction;
+        }
         public bool Equals(Expression expr2)
         {
             return Operation == expr2.Operation && Argument1 == expr2.Argument1 && Argument2 == expr2.Argument2;
@@ -28,34 +67,21 @@ namespace SimpleLang
         {
             return Argument1 == variable || Argument2 == variable;
         }
-
+        public override string ToString()
+        {
+            return instruction.ToString();
+        }
     }
 
-    public class InOutInfo
-    {
-        public Dictionary<BasicBlock, List<Expression>> In { get; set; }
-        public Dictionary<BasicBlock, List<Expression>> Out { get; set; }
-    }
-
-    public class AvailableExpressions
+    public class AvailableExpressions 
     {
         static List<Expression> u;
-        static Dictionary<BasicBlock, List<Expression>> e_gen;
-        static Dictionary<BasicBlock, List<Expression>> e_kill;
-        static Dictionary<BasicBlock, List<Expression>> dictionaryIn;
-        static Dictionary<BasicBlock, List<Expression>> dictionaryOut;
+        public Dictionary<BasicBlock, List<Expression>> e_gen;
+        public Dictionary<BasicBlock, List<Expression>> e_kill;
+        public Dictionary<BasicBlock, List<Expression>> dictionaryIn;
+        public Dictionary<BasicBlock, List<Expression>> dictionaryOut;
 
         List<string> operationTypes = new List<string> { "OR", "AND", "LESS", "PLUS", "MINUS", "MULT", "DIV" };
-                
-        public static InOutInfo Execute(ControlFlowGraph graph)
-        {
-            var optimizer = new AvailableExpressions(graph);
-            optimizer.AssignInitialValues(graph);
-            optimizer.RunIterrations(graph);
-            var result = new InOutInfo() { In = dictionaryIn, Out = dictionaryOut };
-            return result;
-        }
-
         public AvailableExpressions(ControlFlowGraph graph)
         {
             u = new List<Expression>();
@@ -66,9 +92,27 @@ namespace SimpleLang
             Get_e_kill(graph);
             dictionaryIn = new Dictionary<BasicBlock, List<Expression>>();
             dictionaryOut = new Dictionary<BasicBlock, List<Expression>>();
+            AssignInitialValues(graph);
+            RunIterrations(graph);
         }
 
-        void GetU(ControlFlowGraph graph)
+        private void RunIterrations(ControlFlowGraph graph)
+        {
+            var blocks = graph.GetCurrentBasicBlocks();
+            bool changed = true;
+            while (changed)
+            {
+                changed = false;
+                foreach (var block in blocks)
+                {
+                    var parents = GetParents(graph, block);
+                    dictionaryIn[block] = GetIntersection(parents);
+                    dictionaryOut[block] = E_gen_join(block, In_minus_e_kill(block));
+                }
+            }
+        }
+
+        private void GetU(ControlFlowGraph graph)
         {            
             var blocks = graph.GetCurrentBasicBlocks();
             foreach(var block in blocks)
@@ -86,7 +130,7 @@ namespace SimpleLang
             }
         }
 
-        void Get_e_gen(ControlFlowGraph graph)
+        private void Get_e_gen(ControlFlowGraph graph)
         {            
             var blocks = graph.GetCurrentBasicBlocks();
             foreach (var block in blocks)
@@ -109,7 +153,7 @@ namespace SimpleLang
             }
         }
 
-        void Get_e_kill(ControlFlowGraph graph)
+        private void Get_e_kill(ControlFlowGraph graph)
         {
             var blocks = graph.GetCurrentBasicBlocks();
             foreach (var block in blocks)
@@ -132,7 +176,7 @@ namespace SimpleLang
             }            
         }
 
-        List<Expression> GetListOfKilledExpressions(string variable)
+        private List<Expression> GetListOfKilledExpressions(string variable)
         {
             var result = new List<Expression>();
             foreach (var expr in u)
@@ -141,7 +185,7 @@ namespace SimpleLang
             return result;
         }
 
-        bool RemoveExpression(List<Expression> K, Expression expression)
+        private bool RemoveExpression(List<Expression> K, Expression expression)
         {
             for (int i = 0; i < K.Count; i++)
                 if (K[i].Equals(expression))
@@ -151,7 +195,7 @@ namespace SimpleLang
                 }
             return false;
         }
-        void RemoveExpressions(List<Expression> list, string variable)
+        private void RemoveExpressions(List<Expression> list, string variable)
         {
             for (int i = 0; i < list.Count(); i++)
                 if (list[i].ContainsVariable(variable))
@@ -159,7 +203,7 @@ namespace SimpleLang
         }
 
 
-        bool ContainsExpression(List<Expression> list, Expression expr)
+        private bool ContainsExpression(List<Expression> list, Expression expr)
         {
             for (int i = 0; i < list.Count; i++)
                 if (list[i].Equals(expr))
@@ -168,7 +212,7 @@ namespace SimpleLang
         }
 
         
-        void AssignInitialValues(ControlFlowGraph graph)
+        private void AssignInitialValues(ControlFlowGraph graph)
         {
             var blocks = graph.GetCurrentBasicBlocks();
             foreach (var block in blocks)
@@ -178,23 +222,7 @@ namespace SimpleLang
             }
         }
 
-        void RunIterrations(ControlFlowGraph graph)
-        {
-            var blocks = graph.GetCurrentBasicBlocks();
-            bool changed = true; 
-            while(changed)
-            {
-                changed = false;
-                foreach(var block in blocks)
-                {
-                    var parents = GetParents(graph, block);                    
-                    dictionaryIn[block] = GetIntersection(parents);
-                    dictionaryOut[block] = E_gen_join(block, In_minus_e_kill(block));
-                }
-            }
-        }
-
-        List<Expression> GetIntersection(List<BasicBlock> blocks)
+        private List<Expression> GetIntersection(List<BasicBlock> blocks)
         {
             var listOfExpressions = new List<Expression>();
             var result = new List<Expression>();
@@ -222,7 +250,7 @@ namespace SimpleLang
             return result;
         }
 
-        List<Expression> In_minus_e_kill(BasicBlock block)
+        private List<Expression> In_minus_e_kill(BasicBlock block)
         {
             var result = new List<Expression>(dictionaryIn[block]);            
             foreach (var expression in e_kill[block])
@@ -231,7 +259,7 @@ namespace SimpleLang
             return result;
         }
 
-        List<Expression> E_gen_join(BasicBlock block, List<Expression> list)
+        private List<Expression> E_gen_join(BasicBlock block, List<Expression> list)
         {
             var result = new List<Expression>(e_gen[block]);
             foreach (var expression in list)
@@ -240,13 +268,25 @@ namespace SimpleLang
             return result;
         }
 
-        List<BasicBlock> GetParents(ControlFlowGraph graph, BasicBlock block)
+        private List<BasicBlock> GetParents(ControlFlowGraph graph, BasicBlock block)
         {
             var blocksWithID = graph.GetParentsBasicBlocks(block);
             var result = new List<BasicBlock>();
             foreach (var blockWithID in blocksWithID)
                 result.Add(blockWithID.Item2);
             return result;
+        }
+        public class Operation : ICompareOperations<List<Instruction>>
+        {
+            public Operation(List<Instruction> instructions)
+            {
+                this.Upper = new List<Instruction>(instructions);
+            }
+            public List<Instruction> Lower => new List<Instruction>();
+            public List<Instruction> Upper { get; }
+            public bool Compare(List<Instruction> a, List<Instruction> b) => a.SequenceEqual(b);
+            public (List<Instruction>, List<Instruction>) Init() => (Lower, Lower);
+            public List<Instruction> Operator(List<Instruction> a, List<Instruction> b) => a.Union(b).ToList();
         }
     }
 }
