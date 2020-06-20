7bb5be58b36a4e484db55adfc1832fd2e472cd7c
xittz <a.patseev@gmail.com>
2020-06-20 13:27:28 +0300

    Adding test for AST ExprEqualBoolNumIdTest

--- SimpleLanguage.Tests/AST/OptExprEqualBoolNumIdTests.cs
+++ SimpleLanguage.Tests/AST/OptExprEqualBoolNumIdTests.cs
@@ -1,7 +1,31 @@
+using NUnit.Framework;
+using SimpleLang.Visitors;
+
 namespace SimpleLanguage.Tests.AST
 {
-    public class OptExprEqualBoolNumIdTests
+    public class OptExprEqualBoolNumIdTests: ASTTestsBase
     {
-        
+        [Test]
+        public void SumNumTest()
+        {
+            var AST = BuildAST(@"var a, b, c, d;
+a = b == b;
+b = true == true;
+while (5 == 5)
+  c = true == false;
+d = 7 == 8;");
+            var expected = @"var a, b, c, d;
+a = true;
+b = true;
+while true
+  c = false;
+d = false;";
+
+            var opt = new OptExprEqualBoolNumId();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }
     }
 }
