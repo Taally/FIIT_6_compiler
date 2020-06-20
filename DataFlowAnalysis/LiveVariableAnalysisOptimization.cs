52e03d035f7aa986ed5edc4f6d0338a9d7a953fc
tefadivan <43060880+tefadivan@users.noreply.github.com>
2020-06-20 20:49:34 +0300

    Add files via upload

--- DataFlowAnalysis/LiveVariableAnalysisOptimization.cs
+++ DataFlowAnalysis/LiveVariableAnalysisOptimization.cs
@@ -16,22 +16,20 @@ namespace SimpleLang
             var cfg = new ControlFlowGraph(divResult);
             var activeVariable = new LiveVariableAnalysis();
             var resActiveVariable = activeVariable.Execute(cfg);
-            newInstructions = instructions.GetRange(0, instructions.Count - 1);
-            foreach (var x in resActiveVariable)
+            foreach (var x in divResult)
             {
-                foreach (var y in x.Value.Out)
+                var instructionsTemp = x.GetInstructions();
+                newInstructions = instructionsTemp.GetRange(0, instructionsTemp.Count - 1);
+                var InOutTemp = resActiveVariable[x];
+                foreach(var y in InOutTemp.Out)
                 {
-                    if (instructions[instructions.Count-1].Result != y && instructions[instructions.Count - 1].Operation == "assign" && instructions[instructions.Count - 1].Argument1 != y)
+                    if (instructionsTemp[instructionsTemp.Count - 1].Result != y && instructionsTemp[instructionsTemp.Count - 1].Operation == "assign" && instructionsTemp[instructionsTemp.Count - 1].Argument1 != y)
                     {
                         wasChanged = true;
-                    }
-                    else
-                    {
-                        return (wasChanged, newInstructions);
+                        newInstructions.Add(instructionsTemp[instructionsTemp.Count - 1]);
                     }
                 }
             }
-            newInstructions.Add(instructions[instructions.Count - 1]);
             return (wasChanged, newInstructions);
         }
     }
