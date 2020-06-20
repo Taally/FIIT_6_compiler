052351da90c7a74c2c86dbd4d447540e0f6ae843
maxx-1432@yandex.ru <maxx-1432@yandex.ru>
2020-06-20 18:28:13 +0300

    Добавление AST: Оптимизация неравенства констант и тестов к нему

---
+++ SimpleLanguage.Tests/AST/OptExprLessThenRightTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SimpleLang.Visitors;
+
+namespace SimpleLanguage.Tests.AST
+{
+    internal class OptExprLeftLessThenRightTests : ASTTestsBase
+    {
+        [Test]
+        public void SimpleTestWithInts1()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 4 < 13;
+");
+            var expected = @"var c;
+c = true;";
+
+            var opt = new OptExprLeftLessRight();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }
+
+        [Test]
+        public void SimpleTestWithInts2()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 15 < 13;
+");
+            var expected = @"var c;
+c = false;";
+
+            var opt = new OptExprLeftLessRight();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }
+
+        [Test]
+        public void SimpleTestWithBools1()
+        {
+            var AST = BuildAST(@"
+var c;
+c = true < true;
+");
+            var expected = @"var c;
+c = false;";
+
+            var opt = new OptExprLeftLessRight();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }
+
+        [Test]
+        public void SimpleTestWithBools2()
+        {
+            var AST = BuildAST(@"
+var c;
+c = false < true;
+");
+            var expected = @"var c;
+c = true;";
+
+            var opt = new OptExprLeftLessRight();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }
+
+        [Test]
+        public void TestWithIntAndBools1()
+        {
+            var AST = BuildAST(@"
+var b;
+b = (3 < 4) < (true < true);
+");
+            var expected = @"var b;
+b = false;";
+
+            var opt = new OptExprLeftLessRight();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }
+
+        [Test]
+        public void TestWithIntAndBools2()
+        {
+            var AST = BuildAST(@"
+var b;
+b = (5 < 4) < (false < true);
+");
+            var expected = @"var b;
+b = true;";
+
+            var opt = new OptExprLeftLessRight();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }
+
+        [Test]
+        public void TestWithIntAndBools3()
+        {
+            var AST = BuildAST(@"
+var a;
+a = (((3 < 1) < (5 < 2)) < true);
+");
+            var expected = @"var a;
+a = true;";
+
+            var opt = new OptExprLeftLessRight();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }
+
+        [Test]
+        public void TestWithIntAndBools4()
+        {
+            var AST = BuildAST(@"
+var b;
+b = (5 < 4) < false;
+");
+            var expected = @"var b;
+b = false;";
+
+            var opt = new OptExprLeftLessRight();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }        
+
+        [Test]
+        public void TestWithIntAndBools5()
+        {
+            var AST = BuildAST(@"
+var c;
+c = (((3 < 4) < (5 < 7)) < true) < (5 < 6);
+");
+            var expected = @"var с;
+c = false;";
+
+            var opt = new OptExprLeftLessRight();
+            AST.root.Visit(opt);
+            var pp = new PrettyPrintVisitor();
+            AST.root.Visit(pp);
+            Assert.AreEqual(expected, pp.Text);
+        }
+    }
+}
---
+++ Visitors/ExprOptimizations/OptExprLeftLessThenRight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgramTree;
+
+namespace SimpleLang.Visitors
+{
+    public class OptExprLeftLessRight : ChangeVisitor
+    {
+        public override void PostVisit(Node n)
+        {
+            if (n is BinOpNode binop)
+            {
+                if (binop.Left is IntNumNode && binop.Right is IntNumNode && (binop.Left as IntNumNode).Num < (binop.Right as IntNumNode).Num && binop.Op == OpType.LESS)
+                {
+                    ReplaceExpr(binop, new BoolValNode(true));
+                }
+                else
+                if (binop.Left is IntNumNode && binop.Right is IntNumNode && (binop.Left as IntNumNode).Num > (binop.Right as IntNumNode).Num && binop.Op == OpType.LESS)
+                {
+                    ReplaceExpr(binop, new BoolValNode(false));
+                }
+                else
+                if (binop.Left is BoolValNode lbo && binop.Right is BoolValNode rbo && binop.Op == OpType.LESS)
+                {
+                    if (lbo.Val == false && rbo.Val == true)
+                        ReplaceExpr(binop, new BoolValNode(true));
+                    else
+                        ReplaceExpr(binop, new BoolValNode(false));
+                }
+                else
+                {
+                    base.VisitBinOpNode(binop);
+                }
+            }
+        }
+    }
+}
