bcbffb3aa868f95ab9f277838a37fe3339fb94d2
maxx-1432@yandex.ru <maxx-1432@yandex.ru>
2020-06-21 15:26:51 +0300

    AST-оптимизация констант + тесты

---
+++ SimpleLanguage.Tests/AST/OptExprWithOperationsBetweenConsts.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SimpleLang.Visitors;
+
+namespace SimpleLanguage.Tests.AST
+{
+    internal class OptExprWithOperationsBetweenConstsTests : ASTTestsBase
+    {
+        [Test]
+        public void TestOpLess1()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 < 15;
+");
+            var expected = new[] {
+                "var c;",
+                "c = true;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpLess2()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 < 2;
+");
+            var expected = new[] {
+                "var c;",
+                "c = false;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpGreater1()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 > 2;
+");
+            var expected = new[] {
+                "var c;",
+                "c = true;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpGreater2()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 > 4;
+");
+            var expected = new[] {
+                "var c;",
+                "c = false;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpEQGREATER1()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 >= 2;
+");
+            var expected = new[] {
+                "var c;",
+                "c = true;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpEQGREATER2()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 >= 4;
+");
+            var expected = new[] {
+                "var c;",
+                "c = false;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpEQGREATER3()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 >= 3;
+");
+            var expected = new[] {
+                "var c;",
+                "c = true;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpEQLESS1()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 <= 4;
+");
+            var expected = new[] {
+                "var c;",
+                "c = true;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpEQLESS2()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 <= 2;
+");
+            var expected = new[] {
+                "var c;",
+                "c = false;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpEQLESS3()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 <= 3;
+");
+            var expected = new[] {
+                "var c;",
+                "c = true;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpNOTEQUAL1()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 != 2;
+");
+            var expected = new[] {
+                "var c;",
+                "c = true;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void TestOpNOTEQUAL2()
+        {
+            var AST = BuildAST(@"
+var c;
+c = 3 != 3;
+");
+            var expected = new[] {
+                "var c;",
+                "c = false;"
+            };
+
+            var result = ApplyOpt(AST, new OptExprWithOperationsBetweenConsts());
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Visitors/ExprOptimizations/OptExprWithOperationsBetweenConsts.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProgramTree;
+
+namespace SimpleLang.Visitors
+{
+    public class OptExprWithOperationsBetweenConsts : ChangeVisitor
+    {
+        public override void PostVisit(Node n)
+        {
+            if (n is BinOpNode binop)
+            {
+                if (binop.Left is IntNumNode && binop.Right is IntNumNode)
+                {
+                    if ((binop.Left as IntNumNode).Num < (binop.Right as IntNumNode).Num && binop.Op == OpType.LESS)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(true));
+                    }
+                    else
+                    if ((binop.Left as IntNumNode).Num > (binop.Right as IntNumNode).Num && binop.Op == OpType.LESS)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(false));
+                    }
+                    else
+                    if ((binop.Left as IntNumNode).Num > (binop.Right as IntNumNode).Num && binop.Op == OpType.GREATER)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(true));
+                    }
+                    else
+                    if ((binop.Left as IntNumNode).Num < (binop.Right as IntNumNode).Num && binop.Op == OpType.GREATER)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(false));
+                    }
+                    else
+                    if ((binop.Left as IntNumNode).Num >= (binop.Right as IntNumNode).Num && binop.Op == OpType.EQGREATER)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(true));
+                    }
+                    else
+                    if ((binop.Left as IntNumNode).Num < (binop.Right as IntNumNode).Num && binop.Op == OpType.EQGREATER)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(false));
+                    }
+                    else
+                    if ((binop.Left as IntNumNode).Num <= (binop.Right as IntNumNode).Num && binop.Op == OpType.EQLESS)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(true));
+                    }
+                    else
+                    if ((binop.Left as IntNumNode).Num > (binop.Right as IntNumNode).Num && binop.Op == OpType.EQLESS)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(false));
+                    }
+                    else
+                    if ((binop.Left as IntNumNode).Num != (binop.Right as IntNumNode).Num && binop.Op == OpType.NOTEQUAL)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(true));
+                    }
+                    else
+                    if ((binop.Left as IntNumNode).Num == (binop.Right as IntNumNode).Num && binop.Op == OpType.NOTEQUAL)
+                    {
+                        ReplaceExpr(binop, new BoolValNode(false));
+                    }
+                }
+                else
+                {
+                    base.VisitBinOpNode(binop);
+                }
+            }
+        }
+    }
+}
