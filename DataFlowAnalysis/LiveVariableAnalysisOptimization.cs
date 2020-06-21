a0d35a8e6658ed1bd3fd5398cd1b005bdbdd9775
tefadivan <tefad@bk.ru>
2020-06-21 12:25:50 +0300

    ff

--- DataFlowAnalysis/LiveVariableAnalysisOptimization.cs
+++ DataFlowAnalysis/LiveVariableAnalysisOptimization.cs
@@ -12,21 +12,30 @@ namespace SimpleLang
         {
             var wasChanged = false;
             var newInstructions = new List<Instruction>();
-            var divResult = BasicBlockLeader.DivideLeaderToLeader(newInstructions);
+            var divResult = BasicBlockLeader.DivideLeaderToLeader(instructions);
             var cfg = new ControlFlowGraph(divResult);
             var activeVariable = new LiveVariableAnalysis();
             var resActiveVariable = activeVariable.Execute(cfg);
             foreach (var x in divResult)
             {
                 var instructionsTemp = x.GetInstructions();
-                newInstructions = instructionsTemp.GetRange(0, instructionsTemp.Count - 1);
-                var InOutTemp = resActiveVariable[x];
-                foreach(var y in InOutTemp.Out)
+                if (resActiveVariable.ContainsKey(x))
                 {
-                    if (instructionsTemp[instructionsTemp.Count - 1].Result != y && instructionsTemp[instructionsTemp.Count - 1].Operation == "assign" && instructionsTemp[instructionsTemp.Count - 1].Argument1 != y)
+                    var InOutTemp = resActiveVariable[x];
+                    foreach (var i in instructionsTemp)
                     {
-                        wasChanged = true;
-                        newInstructions.Add(instructionsTemp[instructionsTemp.Count - 1]);
+                        if (!InOutTemp.Out.Contains(i.Result) && i.Operation == "assign" && i.Argument1 != i.Result)
+                        {
+                            wasChanged = true;
+                            if (i.Label != "")
+                            {
+                                newInstructions.Add(new Instruction(i.Label, "noop", "", "", ""));
+                            }
+                        }
+                        else
+                        {
+                            newInstructions.Add(i);
+                        }
                     }
                 }
             }
---
+++ SimpleLanguage.Tests/TAC/Simple/LiveVariableAnalysisOptimizationTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SimpleLang;
+
+namespace SimpleLanguage.Tests.TAC.Simple
+{
+    using Optimization = Func<List<Instruction>, (bool, List<Instruction>)>;
+
+    [TestFixture]
+    internal class LiveVariableAnalysisOptimizationTests : TACTestsBase
+    {
+        [Test]
+        public void Test1()
+        {
+            var TAC = GenTAC(@"
+                var a,b,c;
+input (b);
+a = b + 1;
+c = 6;
+if a < c
+	c = b - a;
+else
+	c = b + a;
+print (c);");
+
+            var optimizations = new List<Optimization> { LiveVariableAnalysisOptimization.LiveVariableDeleteDeadCode };
+
+            var expected = new List<string>()
+            {
+                "input b",
+                "#t1 = b + 1",
+                "a = #t1",
+                "#t2 = a < c",
+                "if #t2 goto L1",
+                "#t3 = b + a",
+                "c = #t3",
+                "goto L2",
+                "L1: #t4 = b - a",
+                "c = #t4",
+                "L2: noop",
+                "print c"
+            };
+            var actual = ThreeAddressCodeOptimizer.Optimize(TAC, allCodeOptimizations: optimizations)
+                .Select(instruction => instruction.ToString());
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
