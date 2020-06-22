57f2f828e29d783f22c8dab83808e14f04abf596
Humble Guy <55711450+Tessecrack@users.noreply.github.com>
2020-06-23 00:44:19 +0300

    Update AvailableExpressionsApplicationTests.cs

--- SimpleLanguage.Tests/CFG/AvailableExpressionsApplicationTests.cs
+++ SimpleLanguage.Tests/CFG/AvailableExpressionsApplicationTests.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using NUnit.Framework;
 using SimpleLang;
 using System.Linq;
 using SimpleLang.DataFlowAnalysis;
 
-namespace SimpleLanguage.Tests.DataFlowAnalysis
+namespace SimpleLanguage.Tests.CFG
 {
     [TestFixture]
     internal class AvailableExpressionsApplicationTests : TACTestsBase
@@ -127,7 +127,7 @@ goto 2;
             CollectionAssert.AreEqual(actual, expected);
         }
         [Test]
-        public void ProgramWithLoopFor()
+        public void ProgramWithLoopForNoOptimization1()
         {
             var TAC = GenTAC(@"var a, b, c, d, x, u, e,g, y,zz,i; 
 zz = i + x;
@@ -155,6 +155,76 @@ for i=2,7
                 "L2: noop",
                 "#out: noop",
             };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+       
+        [Test]
+        public void ProgramWithLoopForNoOptimization2()
+        {
+            var TAC = GenTAC(@"var a, b, c, d, x, u, e,g, y,zz,i; 
+e = x + d;
+zz = i + x;
+for i=2,7 
+{
+	x = x + d; 
+	a = a + b;
+}");
+            var availableExpr = GeneratorBasicBlockAfterOptimization(TAC);
+            var actual = GetStringFromBlocks(availableExpr);
+            var expected = new List<string>()
+            {
+"#in: noop",
+"#t1 = x + d",
+"e = #t1",
+"#t2 = i + x",
+"zz = #t2",
+"i = 2",
+"L1: #t3 = i >= 7",
+"if #t3 goto L2",
+"#t4 = x + d",
+"x = #t4",
+"#t5 = a + b",
+"a = #t5",
+"i = i + 1",
+"goto L1",
+"L2: noop",
+"#out: noop",
+            };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void NoOptimizationAfterAllOptimization()
+        {
+            var graph = BuildTACOptimizeCFG(@"var a, b, x, y, z, p, q, s;
+x = y + z;
+y = 1;
+if (a < b)
+{
+    z = 2;
+    p = y + z;
+}
+z = 1;
+q = y + z;");
+            var ae = new AvailableExpressions().Execute(graph);
+            AvailableExpressionsApplication.Execute(graph, ae);
+            var expected = new List<string>()
+            {
+"#in: noop",
+"#t1 = y + z",
+"x = #t1",
+"y = 1",
+"#t2 = a < b",
+"if #t2 goto L1",
+"goto L2",
+"L1: z = 2",
+"#t3 = y + 2",
+"p = #t3",
+"L2: z = 1",
+"#t4 = y + 1",
+"q = #t4",
+"#out: noop"
+            };
+            var actual = GetStringFromBlocks(graph.GetCurrentBasicBlocks());
             CollectionAssert.AreEqual(expected, actual);
         }
     }
