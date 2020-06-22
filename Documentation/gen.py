376d2d952cffb2794e2fa8e86e9341dfc6edf7c3
Dmitry Volodin <mr.molkree@gmail.com>
2020-06-22 06:08:40 +0300

    maybe generate toc using pandoc

--- Documentation/gen.py
+++ Documentation/gen.py
@@ -1,17 +1,17 @@
 import codecs
 
-def gen_toc(path,order):
-    toc = "" # table of content
-    for i, name in enumerate(order):
-        file = codecs.open(path+name+".md", "r", "utf_8_sig")
-        title = file.readline()
-        if (len(title) < 3):
-            title = file.readline()
-        title = title.replace('## ','')
-        title = title.strip()
-        file.close()
-        toc += "[" + str(i+1) + ". " + title + "]" + "(#" + name[2:] + ")\n\n"
-    return toc
+#def gen_toc(path,order):
+#    toc = "" # table of content
+#    for i, name in enumerate(order):
+#        file = codecs.open(path+name+".md", "r", "utf_8_sig")
+#        title = file.readline()
+#        if (len(title) < 3):
+#            title = file.readline()
+#        title = title.replace('## ','')
+#        title = title.strip()
+#        file.close()
+#        toc += "[" + str(i+1) + ". " + title + "]" + "(#" + name[2:] + ")\n\n"
+#    return toc
 
 def main():
     path="Documentation/"
@@ -19,10 +19,10 @@ def main():
     with open(path+"order.txt", "r") as order_file:
         for line in order_file:
             order.append(line.strip())
-    toc = gen_toc(path,order)
+    #toc = gen_toc(path,order)
 
     res = codecs.open(path+"Documentation.md", "w", "utf_8_sig")
-    res.write(toc)
+    #res.write(toc)
     res.write("\n")
     for name in order:
         file = codecs.open(path+name+".md", "r", "utf_8_sig")
