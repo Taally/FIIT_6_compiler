71020e23b69eec74cd1c31f0caa20fb5d25841fd
Николай Моздоров <mozdorov@yandex.ru>
2020-06-23 14:53:43 +0300

    first version

--- TACOptimizations/DeleteDeadCodeWithDeadVars.cs
+++ TACOptimizations/DeleteDeadCodeWithDeadVars.cs
@@ -5,25 +5,44 @@ namespace SimpleLang
 {
     public class DeleteDeadCodeWithDeadVars
     {
-        public static (bool wasChanged, IReadOnlyList<Instruction> instructions) DeleteDeadCode(IReadOnlyList<Instruction> instructions)
+        public static (bool wasChanged, IReadOnlyList<Instruction> instructions) DeleteDeadCode(
+            IReadOnlyList<Instruction> instructions) => DeleteDeadCode(instructions, null);
+
+        public static (bool wasChanged, IReadOnlyList<Instruction> instructions) DeleteDeadCode(
+            IReadOnlyList<Instruction> instructions,
+            IEnumerable<string> liveVariables = null)
         {
             var wasChanged = false;
             var newInstructions = new List<Instruction>();
             var varStatus = new Dictionary<string, bool>();
-
-            var last = instructions.Last();
-            newInstructions.Add(last);
-            varStatus.Add(last.Result, false);
-            if (!int.TryParse(last.Argument1, out _) && last.Argument1 != "True" && last.Argument1 != "False")
+            if (liveVariables != null)
             {
-                varStatus[last.Argument1] = true;
-            }
-            if (!int.TryParse(last.Argument2, out _) && last.Argument2 != "True" && last.Argument2 != "False")
-            {
-                varStatus[last.Argument2] = true;
+                foreach (var variable in liveVariables)
+                {
+                    varStatus[variable] = true;
+                }
             }
 
-            for (var i = instructions.Count - 2; i >= 0; --i)
+            if (liveVariables == null)
+            {
+                var last = instructions.Last();
+                newInstructions.Add(last);
+                varStatus.Add(last.Result, false);
+                if (!int.TryParse(last.Argument1, out _) && last.Argument1 != "True" && last.Argument1 != "False")
+                {
+                    varStatus[last.Argument1] = true;
+                }
+                if (!int.TryParse(last.Argument2, out _) && last.Argument2 != "True" && last.Argument2 != "False")
+                {
+                    varStatus[last.Argument2] = true;
+                }
+            }
+
+            var iStart = liveVariables == null
+                ? instructions.Count - 2
+                : instructions.Count - 1;
+
+            for (var i = iStart; i >= 0; --i)
             {
                 var instruction = instructions[i];
                 if (instruction.Operation == "noop")
@@ -32,7 +51,7 @@ namespace SimpleLang
                     continue;
                 }
                 if (varStatus.ContainsKey(instruction.Result) && !varStatus[instruction.Result]
-                    || instruction.Result.First() == '#' && !varStatus.ContainsKey(instruction.Result))
+                    || instruction.Result.FirstOrDefault() == '#' && !varStatus.ContainsKey(instruction.Result))
                 {
                     newInstructions.Add(new Instruction(instruction.Label, "noop", null, null, null));
                     wasChanged = true;
