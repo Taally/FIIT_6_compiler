ce0e4890a24140c2b00a9b670e49a16b1b626a7c
Ellone <Ukolnir@users.noreply.github.com>
2020-06-23 12:42:50 +0300

    Достигающие определения (#255)

--- IDEForSimpleLang1/Form1.Designer.cs
+++ IDEForSimpleLang1/Form1.Designer.cs
@@ -171,7 +171,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 29);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(481, 456);
+            this.tabPage2.Size = new System.Drawing.Size(514, 456);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Оптимизации по TAC";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -224,7 +224,7 @@
             this.tabPage6.Controls.Add(this.ItOptList);
             this.tabPage6.Location = new System.Drawing.Point(4, 29);
             this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(525, 456);
+            this.tabPage6.Size = new System.Drawing.Size(514, 456);
             this.tabPage6.TabIndex = 2;
             this.tabPage6.Text = "Итерационные алгоритмы";
             this.tabPage6.UseVisualStyleBackColor = true;
@@ -236,7 +236,7 @@
             this.ItOptList.Items.AddRange(new object[] {
             "Доступные выражения",
             "Активные переменные",
-            "Def-Use"});
+            "Достигающие определения"});
             this.ItOptList.Location = new System.Drawing.Point(12, 12);
             this.ItOptList.Name = "ItOptList";
             this.ItOptList.Size = new System.Drawing.Size(466, 361);
